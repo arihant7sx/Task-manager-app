18a9aa66f0eeed2d9b6467c4d80f17c999943984
arihant <arihanttyagi78@gmail.com>
2025-06-01 12:07:53 +0530

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+FILEPATH = "tasks.txt"
+
+
+def get_tasks(filepath=FILEPATH):
+    """ Reads a text file and
+    returns the list of tasks """
+    with open(filepath, 'r') as file_local:
+        tasks_local = file_local.readlines()
+    return tasks_local
+
+def write_tasks(tasks_arg, filepath=FILEPATH):
+    """ Write the tasks in tasks list"""
+    with open(filepath, 'w') as file:
+        file.writelines(tasks_arg)
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_tasks())
--- main.py
+++ main.py
@@ -1,25 +1,68 @@
-tasks = []
+import functions
+import time
+
+now = time.strftime("%d-%y-%m %H:%M:%S")
+print("It is: ",now)
 while True:
-    user_choice = input("Type add, show, edit, complete or exit")
+    user_choice = input("Type add, show, edit, complete or exit: ")
+    user_choice  = user_choice.strip()
 
-    match user_choice.strip():
-        case 'add':
-            task = input("enter a task")
-            tasks.append(task)
-        case 'show' :
-            for index,item in enumerate(tasks):
-                item=item.title()
-                print(f"{index+1}-{item}")
-        case 'edit':
-            number = int(input("number of the task to edit: "))
+    if user_choice.startswith("add"):
+        task = user_choice[4:]
+
+        tasks = functions.get_tasks()
+
+        tasks.append(task + '\n')
+
+        functions.write_tasks(tasks,)
+
+    elif user_choice.startswith("show"):
+
+        tasks = functions.get_tasks()
+        for index,item in enumerate(tasks):
+            item = item.strip('\n')
+            row = f"{index+1}-{item}"
+            print(row)
+
+    elif user_choice.startswith("edit"):
+        try:
+            number = int(user_choice[5:])
+            print(number)
+
             number = number - 1
-            new_task = input("Enter the new task")
-            tasks[number] = new_task
-        case 'complete':
-            number=int(input("Enter the number of the completed task"))
-            tasks.pop(number-1)
 
-        case 'exit':
-            break
+            tasks = functions.get_tasks()
 
-print("Adios!,")
+            new_task = input("Enter the new task: ")
+            tasks[number] = new_task + '\n'
+
+            functions.write_tasks(tasks,)
+        except ValueError:
+            print("Invalid command")
+            continue
+
+
+    elif user_choice.startswith("complete"):
+        try:
+            number=int(user_choice[9:])
+            tasks = functions.get_tasks()
+            index = number-1
+            task_to_remove=tasks[index].strip('\n')
+            tasks.pop(index)
+            functions.write_tasks(tasks,)
+
+            message=f"Task {task_to_remove} has been removed from the list"
+            print(message)
+        except IndexError:
+            print("No task with such number")
+            continue
+        except ValueError:
+            print("Add the number of task to be removed")
+            continue
+
+    elif user_choice.startswith("exit"):
+        break
+
+    else:
+        print("Invalid command!")
+print("Adios!")

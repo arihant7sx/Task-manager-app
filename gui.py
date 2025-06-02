3f72cf87ed359e5198d6cbee7317b464e461ed5d
arihant <arihanttyagi78@gmail.com>
2025-06-02 12:17:46 +0530

    Crete basic GUI without interaction

--- gui.py
+++ gui.py
@@ -1,1 +1,10 @@
+import FreeSimpleGUI as sg
 import functions
+
+label = sg.Text("Type in a task")
+input_box = sg.InputText(tooltip="Enter task")
+add_button = sg.Button("Add")
+
+window = sg.Window("MY To-Do App", layout = [[label], [input_box,add_button]])
+window.read()
+window.close()
